91661f27a7f438d00bb2972a8dd568ea61b87e5d
ceciliehermansen <cecilie.haugsbo@gmail.com>
2017-01-10 19:33:21 +0100

    lag til contains og gjort litt ryddigere

--- foreksempel/jpages/py2javaLists.py
+++ foreksempel/jpages/py2javaLists.py
@@ -3,19 +3,30 @@ list = ["hei", "paa", "deg", "hva", "skjer"]
 print(list)  # Printer listen
 
 #Diverse listeoperasjoner
-print(list[0])  # frste element
-print(list[-1])  # siste element
+print(len(list))  # lengde
+
+print(list[0])  # forste element
+print(list[-1])  # Eventuelt list[len(list)-1]   siste element
 print(list[3:])  # subliste med elementer fra indeks 3 og ut lista
 print(list[0:2])  # subliste med element 0 og 1
 print(list[-2:]) # subliste med 2 siste elementer
 
-print(len(list))  # lengden til listen
-print(list.pop(0))  # returnerer og fjerner frste element
-print(list.remove("paa"))  # fjerner elemtet paa 
-print(list.append("java"))  # Legger til 6 paa slutten av listen
+
+list.pop(0)  # Fjerner og returnerer forste element
 print(list)
 
-print(list.index("java"))   # Indeks til element
+list.remove("paa") # fjerner elemtet paa 
 print(list)
+
+list.append("java")  # Legger til 6 paa slutten av listen
+print(list)
+
+list[0]="meg"  # setter "meg" paa index 0
+print(list)
+
+print( "meg" in list)  # Sjekker om listen inneholder elementet "meg"
+
+print(list.index("java"))   # Indeks til element
+
 
 
