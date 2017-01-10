ea01e1536f44864aab5b7dc0d772849052223efa
Vemund M Santi <vemund@santi.no>
2017-01-10 10:33:13 +0100

    Add number examples

---
+++ foreksempel/jpages/py2javaNumbers.py
@@ -0,0 +1,36 @@
+# Methods with doubles and integers
+dou = 2.2
+print(dou)
+
+douString = str(dou)
+print(douString)
+
+dou2 = float(douString)
+print(dou2)
+
+i = 2
+d = 2.0
+print(i == d) # True
+
+d = float(i)  # Returns the value of i as a float (the same as a double)
+
+# Comparing numbers
+d1 = 5.6
+d2 = 3.2
+print(d1 > d2)  # true
+print(d1 < d2)  # false
+
+# Increase/decrease the value of numbers
+d = 4.5
+print(d)  # 4.5
+d += 1
+print(d)  # 5.5
+d -= 1
+print(d)  # 4.5
+
+i = 4
+print(i)  # 4
+i += 1
+print(i)  # 5
+i -= 1
+print(i)  # 4
