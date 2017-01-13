04f6c65940b44dcf9a6da95745f7f81b07021aba
Hallvard Traetteberg <hal@idi.ntnu.no>
2017-01-13 15:42:46 +0100

    Kommentarer i Java- og FXML-koden

--- foreksempel/src/average/Average.java
+++ foreksempel/src/average/Average.java
@@ -2,15 +2,25 @@ package average;
 
 public class Average {
 
-	int valueCount = 0;
-	double sum = 0.0;
+	// to objekt-variabler (også kalt attributer eller felt)
 	
+	// int er typen for heltall, så valueCount kan bare bli satt til heltall 
+	int valueCount = 0;
+	// double er typen for desimaltall, så sum kan bare bli satt til desimaltall
+	// (heltall blir konvertert, om nødvendig)
+	double sum = 0.0;
+
+	// metodedeklarasjon, void betyr at metoden ikke returnerer noe
 	void acceptValue(double value) {
+		// legg value til sum (og oppdater sum)
 		sum = sum + value;
+		// legg 1 til valueCount (og oppdater valueCount)
 		valueCount = valueCount + 1;
 	}
 	
+	// metodedeklarasjon, returnerer en double, altså et desimaltall
 	double getAverage() {
+		// returner resultatet av beregningen, double delt på int blir en double
 		return sum / valueCount;
 	}
 }
--- foreksempel/src/average/AverageController.java
+++ foreksempel/src/average/AverageController.java
@@ -6,23 +6,40 @@ import javafx.scene.control.TextField;
 
 public class AverageController {
 	
+	// referanse til et Average-objekt
 	Average average;
 	
+	// kalles etter at FXML-mekanismen har laget alle JavaFX-objektene og
+	// koblet JavaFX-objekter med fx:id-er med tilsvarende attributter
+	// @FXML angir at metoden er ment å bli kalt av FXML-mekanismen
 	@FXML
 	void initialize() {
+		// sett average-variablen til et nytt Average-objekt
 		average = new Average();
 	}
 
+	// referanse til et Label-objekt, som er et JavaFX-objekt for tekst-output
+	// @FXML angir at attributtet er ment å bli satt av FXML-mekanismen
 	@FXML
 	Label averageOutput;
-	@FXML TextField acceptValueInput;
+	// referanse til et TextField-objekt, som er et JavaFX-objekt for tekst-input
+	// @FXML angir at attributtet er ment å bli satt av FXML-mekanismen
+	@FXML
+	TextField acceptValueInput;
 
+	// metode som kalles når acceptValue-knappen trykkes
+	// 
 	@FXML
 	public void handleAcceptValue() {
+		// hent teksten (en String) ut av tekstfeltet
 		String acceptValueString = acceptValueInput.getText();
-		double v = Double.valueOf(acceptValueString);
-		average.acceptValue(v);
+		// konverter teksten til et desimaltall
+		double value = Double.valueOf(acceptValueString);
+		// gi desimaltallet til Average-objektet
+		average.acceptValue(value);
+		// be om gjennomsnittet og konverter til tekst (en String)
 		String s = String.valueOf(average.getAverage());
+		// oppdater Label-objektet
 		averageOutput.setText(s);
 	}	
 }
