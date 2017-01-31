90abaf795e9c99b6ba99a475182ad26c6446a6ac
Vemund M Santi <vemund@santi.no>
2017-01-31 13:56:25 +0100

    Add programming tasks for assignment 3

---
+++ ovinger/tests/encapsulation/Card.ex
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xmi:XMI xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:exercise="platform:/plugin/no.hal.learning.exercise.model/model/exercise.ecore" xmlns:jdt="platform:/plugin/no.hal.learning.exercise.jdt/model/jdt-exercise.ecore" xmlns:junit="platform:/plugin/no.hal.learning.exercise.junit/model/junit-exercise.ecore" xmlns:workbench="platform:/plugin/no.hal.learning.exercise.workbench/model/workbench-exercise.ecore">
+  <exercise:Exercise>
+    <parts xsi:type="exercise:ExercisePart" title="Card">
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Write source code for the Card class."/>
+        <a xsi:type="jdt:JdtSourceEditAnswer" className="encapsulation.Card"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Write source code for the CardDeck class."/>
+        <a xsi:type="jdt:JdtSourceEditAnswer" className="encapsulation.CardDeck"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Run the Card JUnit test."/>
+        <a xsi:type="junit:JunitTestAnswer" testRunName="encapsulation.CardTest"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Run the CardDeck JUnit test."/>
+        <a xsi:type="junit:JunitTestAnswer" testRunName="encapsulation.CardDeckTest"/>
+      </tasks>
+    </parts>
+    <parts xsi:type="exercise:ExercisePart" title="Tool usage">
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use breakpoints to debug code."/>
+        <a xsi:type="workbench:DebugEventAnswer" action="suspend.breakpoint"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use the debug command Step Over"/>
+        <a xsi:type="workbench:CommandExecutionAnswer" elementId="org.eclipse.debug.ui.commands.StepOver" action="executeSuccess"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use the debug command Step Into"/>
+        <a xsi:type="workbench:CommandExecutionAnswer" elementId="org.eclipse.debug.ui.commands.StepInto" action="executeSuccess"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Use the Variables view"/>
+        <a xsi:type="workbench:PartTaskAnswer" elementId="org.eclipse.debug.ui.VariableView" action="activated"/>
+      </tasks>
+    </parts>
+  </exercise:Exercise>
+</xmi:XMI>
