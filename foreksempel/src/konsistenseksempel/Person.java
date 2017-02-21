6d0791be600045f630574da37ab34f05f0204242
Hallvard Traetteberg <hal@ntnu.no>
2017-02-21 16:13:26 +0100

    Konsistenseksempel

---
+++ foreksempel/src/konsistenseksempel/Person.java
@@ -0,0 +1,63 @@
+package konsistenseksempel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Person {
+
+	private String name;
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "[Person " + name + "]";
+	}
+	
+	private Collection<Pet> pets = new ArrayList<Pet>();
+
+	public boolean hasPet(Pet pet) {
+		return pets.contains(pet);
+	}
+	
+	public void addPet(Pet pet) {
+		if (hasPet(pet)) {
+			return;
+		}
+		pets.add(pet);
+		pet.setOwner(this);
+	}
+
+	public void removePet(Pet pet) {
+		if (! hasPet(pet)) {
+			return;
+		}
+		pets.remove(pet);
+		pet.setOwner(null);
+	}
+
+	public static void main(String[] args) {
+		Pet pet1 = new Pet("Wario");
+		Person person1 = new Person("Hallvard");
+		Person person2 = new Person("Jens");
+		
+		pet1.setOwner(person1);
+		System.out.println(pet1.getOwner() == person1);
+		System.out.println(person1.hasPet(pet1));
+		
+		pet1.setOwner(person2);
+		System.out.println(pet1.getOwner() == person2);
+		System.out.println(! person1.hasPet(pet1));
+		System.out.println(person2.hasPet(pet1));
+		
+		pet1.setOwner(null);
+		System.out.println(pet1.getOwner() == null);
+		System.out.println(! person2.hasPet(pet1));
+		
+		person1.addPet(pet1);
+		System.out.println(pet1.getOwner() == person1);
+		System.out.println(person1.hasPet(pet1));
+	}
+}
---
+++ foreksempel/src/konsistenseksempel/Pet.java
@@ -0,0 +1,31 @@
+package konsistenseksempel;
+
+public class Pet {
+	
+	private String name;
+	
+	public Pet(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "[Pet " + name + "]";
+	}
+	
+	private Person owner;
+	
+	public Person getOwner() {
+		return owner;
+	}
+	
+	public void setOwner(Person owner) {
+		if (this.owner != null) {
+			this.owner.removePet(this);
+		}
+		this.owner = owner;
+		if (this.owner != null) {
+			this.owner.addPet(this);
+		}
+	}
+}
